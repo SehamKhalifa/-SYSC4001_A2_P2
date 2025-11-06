7fc0bf593b3a133d13ce90d9621e7ff6729e135a
SehamKhalifa <sehamkhalifa@cmail.carleton.ca>
2025-11-06 18:38:07 -0500

    fixed cycle_counter to do what's intended for q1-3

--- process2.c
+++ process2.c
@@ -5,24 +5,24 @@
 
 int main(void) {
     printf("Process 2 (child, PID %d) started\n", getpid());
-    long cycle = 0;
+    long cycle_counter = 0;
     
     while (1) {
         // Check if cycle is a multiple of 3
-        if (cycle % 3 == 0) {
-            printf("Process 2 - Cycle number: %ld - %ld is a multiple of 3\n", cycle, cycle);
+        if (cycle_counter % 3 == 0) {
+            printf("Process 2 - Cycle number: %ld - %ld is a multiple of 3\n", cycle_counter, cycle_counter);
         } else {
-            printf("Process 2 - Cycle number: %ld\n", cycle);
+            printf("Process 2 - Cycle number: %ld\n", cycle_counter);
         }
 
         // Check if reached the exit condition
-        if (cycle <= -500) {
-            printf("\nProcess 2: Reached %ld (lower than -500)\n", cycle);
+        if (cycle_counter <= -500) {
+            printf("\nProcess 2: Reached %ld (lower than -500)\n", cycle_counter);
             printf("Process 2: Exiting now...\n");
             exit(0);  // Exit with status 0 (success)
         }
 
-        cycle--;
+        cycle_counter--;
         usleep(500000);   // sleep 0.5s to slow output
     }
 
