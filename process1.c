ee4d604cc320ec2fdeab455f584e32d93618e2e5
Nam Duong <namduong@cmail.carleton.ca>
2025-11-07 15:47:07 -0500

    Add files via upload

--- process1.c
+++ process1.c
@@ -2,7 +2,36 @@
 #include <unistd.h>  
 #include <sys/types.h>  
 #include <stdlib.h>  
-#include <sys/wait.h>    
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+typedef struct {
+    int mult;
+    int count;
+} shmem_t;
+
+union semun { // Sometimes necessary for semctl
+    int val;
+    struct semid_ds *buf;
+    unsigned short  *array;
+};
+
+void sema_wait(int semaid){
+    struct sembuf sema_buffer = {0, -1, 0};
+    if(semop(semaid, &sema_buffer, 1) == -1){
+        perror("semop wait failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sema_sig(int semaid){
+    struct sembuf sema_buffer = {0, 1, 0};
+    if(semop(semaid, &sema_buffer, 1) == -1){
+        perror("semop signal failed");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(void) {
     pid_t pid = fork();   // create new process
@@ -25,10 +54,55 @@ int main(void) {
         
         sleep(0.1);  // Give child time to start
 
-        long counter = 0;  
-        long cycle = 0;    // Cycle counter
+        int counter = 0;  
         int status;
         pid_t result;
+
+        key_t key_mem = ftok(".", 'A'); // Generate key
+        if (key_mem == -1) {
+            perror("mem ftok failed");
+            exit(EXIT_FAILURE);
+        }
+
+        int shmemid = shmget(key_mem, sizeof(shmem_t), IPC_CREAT | 0666); // Create shared memory space
+        if (shmemid == -1) {
+            perror("shmget failed");
+            exit(EXIT_FAILURE);
+        }
+
+        shmem_t *shmem = (shmem_t *) shmat(shmemid, (void *) NULL, 0); // Attach to shared memory
+            if (shmem == -1) {
+            perror("shmat failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Process 1: Shared memory successfully initialized\n");
+
+        key_t key_sema = ftok(".", 'B'); // Generate key
+        if (key_sema == -1) {
+            perror("sema ftok failed");
+            exit(EXIT_FAILURE);
+        }
+
+        int semaid = semget(key_sema, 1, IPC_CREAT | 0666); // Create semaphore
+        if(semaid == -1){
+            perror("semget failed");
+            exit(EXIT_FAILURE);
+        }
+
+        union semun sema_union; // Initialize semaphore to be open
+        sema_union.val = 1;
+        if (semctl(semaid, 0, SETVAL, sema_union) == -1) {
+            perror("semctl SETVAL failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Process 1: Semaphore successfully initialized\n");
+ 
+        sema_wait(semaid);
+        shmem->mult = 5;
+        shmem->count = 0;
+        sema_sig(semaid);
 
         while (1) {
             // Check if Process 2 finished
@@ -40,24 +114,40 @@ int main(void) {
                 if (WIFEXITED(status)) {
                     printf("Process 1: Process 2 exited with status %d\n", WEXITSTATUS(status));
                 }
-                printf("Process 1: Ending now...\n");
+                printf("Process 1: Exiting now...\n");
                 break;
             } else if (result < 0) {
                 perror("waitpid failed");
                 break;
             }
 
-            // Increment counter and print only multiples of 3
-            if (counter % 3 == 0) {
-                printf("Cycle number: %ld — %ld is a multiple of 3\n", cycle, counter);
+            sema_wait(semaid);
+            // Increment counter and print only multiples of mult chosen
+            if (counter % shmem->mult == 0) {
+                printf("Process No 1 - Cycle number: %d — %d is a multiple of %d\n", counter, counter, shmem->mult);
             } else {
-                printf("Cycle number: %ld\n", cycle);
+                printf("Process No 1 - Cycle number: %d\n", counter);
+            }
+
+            if(counter > 500){
+                sema_sig(semaid);
+                printf("\nProcess 1: Reached 500\n");
+                printf("Process 1: Exiting now...\n");
+                exit(0);
             }
 
             counter++;
-            cycle++;
-            sleep(0.5);   // 0.5 seconds delay
+            shmem->count = counter;
+            sema_sig(semaid);
+            usleep(100000);   // 0.5 seconds delay
         }
+
+        sleep(3); // To ensure process 2 finishes first
+
+        shmdt(shmem); // Clean up
+        shmctl(shmemid, IPC_RMID, (void*) NULL);
+        semctl(semaid, 0, IPC_RMID);
+        printf("Process 1: Shared memory and sempahore succesfully cleaned up\n");
     }
 
     return 0;
